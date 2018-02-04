0eead9b249022012f55a482341ca7713f4010717
vigneshwari <vigneshwariraja84@gmail.com>
2018-02-04 23:50:15 +0530

    propertyfile change

--- config/src/main/java/com/config/server/ConfigAppInit.java
+++ config/src/main/java/com/config/server/ConfigAppInit.java
@@ -1,10 +1,10 @@
 package com.config.server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootConfiguration
+@SpringBootApplication
 @EnableConfigServer
 public class ConfigAppInit {
 
